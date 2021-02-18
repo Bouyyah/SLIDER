8d0dc8c90295108facede5e6670b9b194681cf1f
bouyyah <hamameayyoub@gmail.com>
2021-02-18 16:59:54 +0100

    update functionalities

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let slideWidth = document.getElementById("game").offsetWidth/2;
 console.log(slideWidth);
 let count = 1;
 let bottom = 35;
+let arrange = 15;
+let constArrange = 15;
 let sliderCurrent = createSlider(count);
 
 
@@ -19,12 +21,27 @@ function getSliderBelow(){
     return (count==1) ? sliderCurrent : document.getElementById("slider".concat(count-1));
 }
 
+function deleteSlide(numToDelete){
+    for (let i = (arrange-1) ; i > 0; i--){
+       
+        var sld = document.getElementById("slider"+(numToDelete-i)); 
+        if (sld!=null) {
+            sld.remove();
+        }
+          
+    }
+    sliderCurrent.style.bottom = "0px";
+    bottom = 35;
+    arrange = arrange + constArrange;
+    
+}
+
 function stopSliding(){
 
     var left = window.getComputedStyle(sliderCurrent).getPropertyValue("left");
     sliderCurrent.classList.remove("animate");
     sliderCurrent.style.left = left;
-
+    
     var sliderBelow = getSliderBelow();
     var width = parseInt(window.getComputedStyle(sliderCurrent).getPropertyValue("width"));
     var leftBelow = parseInt(window.getComputedStyle(sliderBelow).getPropertyValue("left"));
@@ -38,6 +55,11 @@ function stopSliding(){
         if(!alert(score)){window.location.reload();}
     } 
 
+    document.getElementById("begin").innerHTML = count.toString();
+
+    if (count === arrange) {
+        deleteSlide(count);
+    }
 
     if (difference > 0) {
         left = left + absDifference;
@@ -46,8 +68,8 @@ function stopSliding(){
         sliderCurrent.style.left = left.toString().concat("px");
     }
 
-    count++;
-    let sliderAbove = createSlider(count);
+   
+    let sliderAbove = createSlider(count+1);
     let offset = (width - absDifference).toString().concat("px");
     sliderCurrent.style.width = offset;
     sliderAbove.style.width = offset;  
@@ -56,4 +78,6 @@ function stopSliding(){
     slideWidth = slideWidth + absDifference;
     document.documentElement.style.setProperty("--width",(slideWidth+"px"));
     sliderCurrent = sliderAbove;
+    count++;
+    
 }
